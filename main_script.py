cf968e1fffa71931846522595f795aea9c5d060e
gengler1123 <gengler@stevens.edu>
2016-08-14 14:51:35 -0400

    Updated

--- main_script.py
+++ main_script.py
@@ -12,7 +12,8 @@ import NeuronParameters as NP
 from random import random
 from Structure.NetworkStimulation import NetworkStimulation
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 NetGen = NG.NetworkGeneration()
 
@@ -80,7 +81,7 @@ for i in Net.nodes():
 
 print(maxDelay)
 
-RUNS = 6
+RUNS = 60
 
 for RUN in range(RUNS):
     input_neurons = NetworkStimulation(Net, random()*xmax, random()*ymax, .4)
@@ -95,24 +96,32 @@ for RUN in range(RUNS):
                 Net.node[n]['model'].clamp_input(I_clamp)
             Net.NetTimestep()
     
-    Net.print_Firings()
+    #Net.print_Firings()
     X.append(Net.Firings)
     Net.Firings=np.array([[0,0,None,None]])
     Net.t = 0
 
 PSPF = []
+VON = []
 PM = []
+
 
 tau = 5
 
 for I in X:
     PSPF.append([])
+    VON.append([])
     for n in Net.nodes():
         PSPF[-1].append(np.zeros(T))
+        VON[-1].append(np.zeros(T))
     for i,x in enumerate(I):
         if i != 0:
             t = int(x[0])
             n = int(x[1])
+            for z in range(20):
+                if t + z < T:
+                    VON[-1][n][t+z] += np.exp(-z/tau)
+                pass
             for m in Net.successors(n):
                 w = Net[n][m]['weight']
                 d = Net[n][m]['delay']
@@ -123,31 +132,79 @@ for I in X:
 
 DATA = {}
 DISTANCE = {}
+NAIVE = {}
+NAIVEDIST={}
 
 for R1 in range(RUNS):
     for R2 in range(RUNS):
         if R2 > R1:
             PM = []
+            NM = []
             for n in Net.nodes():
                 PM.append((PSPF[R1][n] - PSPF[R2][n])**2)
+                NM.append((VON[R1][n] - VON[R2][n])**2)
                 pass
             try:
                 DATA[R1][R2] = PM
+                NAIVE[R1][R2] = NM
                 DISTANCE[R1][R2] = 0
+                NAIVEDIST[R1][R2] = 0
                 VALUE = 0
+                NVAL = 0
                 for N in PM:
                     VALUE += sum(N)
                 DISTANCE[R1][R2] = np.sqrt(VALUE)
+                for N in NM:
+                    NVAL += sum(N)
+                NAIVEDIST[R1][R2] = np.sqrt(NVAL)
                 
             except:
                 DATA[R1] = {}
                 DATA[R1][R2] = PM
+                
                 DISTANCE[R1] = {}
                 DISTANCE[R1][R2] = 0
+                
+                NAIVE[R1] = {}
+                NAIVE[R1][R2] = NM
+                
+                NAIVEDIST[R1] = {}
+                NAIVEDIST[R1][R2] = 0
+                
                 VALUE = 0
+                NVAL = 0
+                
                 for N in PM:
                     VALUE += sum(N)
                 DISTANCE[R1][R2] = np.sqrt(VALUE)
+                
+                for N in NM:
+                    NVAL += sum(N)
+                NAIVEDIST[R1][R2] = np.sqrt(NVAL)
+                
             pass
+
+RANGE = np.linspace(0,T,T)
+
+print("Pseudometric Distances")
+A = []
+B = []
+
+
+for i in range(RUNS):
+    for j in range(RUNS):
+        if j > i:
+            print(DISTANCE[i][j])
+            A.append(DISTANCE[i][j])
+
+print("Naive Distances")
+
+for i in range(RUNS):
+    for j in range(RUNS):
+        if j > i:
+            print(NAIVEDIST[i][j])
+            B.append(NAIVEDIST[i][j])
+
+
 
 
